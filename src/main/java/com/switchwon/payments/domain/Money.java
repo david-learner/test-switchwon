f4736daf004b10ac54e7666a0f6105b601c347ad
david-learner <david.hwang@sodacrew.com>
2024-06-15 17:05:24 +0900

    add redefined equals & hashcode to Money

--- src/main/java/com/switchwon/payments/domain/Money.java
+++ src/main/java/com/switchwon/payments/domain/Money.java
@@ -2,6 +2,7 @@ package com.switchwon.payments.domain;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.Objects;
 
 public class Money {
 
@@ -19,5 +20,34 @@ public class Money {
 
     public BigDecimal getAmount() {
         return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "currency=" + currency +
+                ", amount=" + amount +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return Objects.equals(currency, money.currency) && equalsAmount(amount, money.amount);
+    }
+
+    private boolean equalsAmount(BigDecimal amount1, BigDecimal amount2) {
+        return amount1.compareTo(amount2) == 0 ? true : false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currency, amount);
     }
 }
